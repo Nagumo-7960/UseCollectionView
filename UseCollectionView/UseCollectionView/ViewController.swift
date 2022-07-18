df73a86b25884de879e53aa34523004759fb8acd
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-07-18 13:35:55 +0900

    画像を自前のものに変更

--- UseCollectionView/UseCollectionView/ViewController.swift
+++ UseCollectionView/UseCollectionView/ViewController.swift
@@ -32,7 +32,7 @@ struct User:Codable{
 
 var qiitaUserName = ""
 //最初にnilだとエラーを吐いてしまうので、一時的に入れている画像URL
-var imageURL = "https://www.webantena.net/wp-content/uploads/2013/12/loading-line.gif"
+var imageURL = "https://user-images.githubusercontent.com/69156255/179443993-07261c4d-4a3f-47e6-9a21-6d8def61f91e.gif"
 
 class ViewController: UIViewController {
     private var qiitas = [Qiita]()
